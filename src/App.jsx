6bd02a0af1e729e504e9fef381bfec3b6a5c3748
karthickb2210 <116095490+karthickb2210@users.noreply.github.com>
2024-08-10 11:18:33 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+import Carousel from './Components/Carousel/Carousel'
+import Featured from './Components/Featured/Featured'
+import Footer from './Components/Footer/Footer'
+import Navbar from './Components/NavBar/Navbar'
+import Process from './Components/Process/Process'
+
+function App() {
+  
+  return (
+    <>
+      <Navbar />
+    <Carousel/>
+    <Featured />
+    <Process />
+    <Footer />
+    </>
+  )
+}
+
+export default App
