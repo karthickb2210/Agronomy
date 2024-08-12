23399f87a2a781547a3b6c11f406ff9cde600256
karthickb2210 <116095490+karthickb2210@users.noreply.github.com>
2024-08-12 10:00:48 +0530

    added dummy route

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import { BrowserRouter,Routes,Route } from 'react-router-dom'
 import MainPage from '../Components/HomePage/MainPage'
-import Shop from '../Components/Shop/Shop'
+import Shop from '../Components/Shop/Shop.jsx'
+import Featured from '../Components/HomePage/Featured/Featured.jsx'
 export default function Layout() {
   return (
     <>
@@ -9,7 +10,8 @@ export default function Layout() {
              
             <Routes>
                 <Route path='/' element={<MainPage />}></Route>
-                <Route path='/store' element={<Shop />}> </Route>
+                <Route path='/store' element={<Shop />}></Route>
+                <Route path="/fet" element={<Featured />}></Route>
             </Routes>
             
         </BrowserRouter>
